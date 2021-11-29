9034fa4a5ed15ad590be06924243a93bb5eff96a
Ron Salm <rsalm@oceaneering.com>
2021-11-29 18:07:13 +0000

    docs: Documented Visistor methods

--- include/ReaderVisitor.h
+++ include/ReaderVisitor.h
@@ -1,7 +1,25 @@
 #pragma once
 
+/**
+ * @Brief Reader visitor
+ *
+ * This visitor is used to provide new laptime info
+ */
 class ReaderVisitor {
 public:
-  virtual void addRecord(int, int) = 0;
+
+  /**
+   * Provide new data to the invoker of the reader
+   *
+   * @param kartId The id of the kart
+   * @param laptime The timestamp of the kart passing the start/finish line in seconds
+   */
+  virtual void addRecord( int kartId, int laptime) = 0;
+
+  /** 
+   * Allow the reader to verify whether the invoker allows more data
+   *
+   * @return Invoker is done fetching data [True = Stop reading]
+   */
   virtual bool isFinished() = 0;
 };
